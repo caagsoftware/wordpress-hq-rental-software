3600a961ea3c05405de163119bcf476e5dcc8fed
Faggioni <miguelfaggioni@gmail.com>
2017-09-12 21:39:01 -0400

    Init Repo

---
+++ caag-car-rental.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name:  Caag Car Rental
+Plugin URI:   https://caagsoftware.com
+Description:  Basic WordPress Plugin Header Comment
+Version:      0.1
+Author:       Miguel Faggioni
+Author URI:   https://caagsoftware.com
+*/

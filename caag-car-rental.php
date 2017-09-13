9687295a3e1be8fac68fd06dabbb60cb1d0eb767
Faggioni <miguelfaggioni@gmail.com>
2017-09-12 21:41:42 -0400

    Init Repo

--- caag-car-rental.php
+++ caag-car-rental.php
@@ -7,3 +7,10 @@ Version:      0.1
 Author:       Miguel Faggioni
 Author URI:   https://caagsoftware.com
 */
+
+/*
+ * Global Constant
+ */
+define('CAAG_RENTAL_VERSION','0.1');
+define('CAAG_RENTAL_POST_TYPE','caag-rental-form');
+

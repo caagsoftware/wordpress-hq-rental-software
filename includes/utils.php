be5c34532bf6ef27390831eff07f0042b6d7f334
Nadin Yamaui <nadin@caagcrm.com>
2017-12-07 16:24:58 -0400

    Use the right function for the plugin assets

--- includes/utils.php
+++ includes/utils.php
@@ -6,7 +6,7 @@
  */
 function caag_hq_rental_styles()
 {
-    wp_register_style('caag-rental', plugins_url(CAAG_HQ_RENTAL_PLUGIN_FOLDER . '/assets/css/caag_hq_rental.css'));
+    wp_register_style('caag-rental', plugin_dir_url(__FILE__) . 'assets/css/caag_hq_rental.css');
     wp_enqueue_style('caag-rental');
 }
 
@@ -19,9 +19,8 @@ add_action('caag_hq_rental_styles', 'caag_hq_rental_styles');
 function caag_hq_rental_scripts()
 {
     wp_register_script('caag-rental-iframe-resize',
-        plugins_url(CAAG_HQ_RENTAL_PLUGIN_FOLDER . '/assets/js/iframeSizer.min.js?version=3.5.15'));
-    wp_register_script('caag-rental-iframe-init',
-        plugins_url(CAAG_HQ_RENTAL_PLUGIN_FOLDER . '/assets/js/caagResize.js'), array('jquery'));
+        plugin_dir_url(__FILE__) . 'assets/js/iframeSizer.min.js?version=3.5.15');
+    wp_register_script('caag-rental-iframe-init', plugin_dir_url(__FILE__) . 'assets/js/caagResize.js', ['jquery']);
     wp_enqueue_script('caag-rental-iframe-resize');
     wp_enqueue_script('caag-rental-iframe-init');
 }
@@ -35,8 +34,8 @@ add_action('wp_enqueue_script', 'caag_hq_rental_scripts');
 */
 function caag_hq_rental_inline_script()
 {
-    wp_enqueue_script('caag-rental-script-submit', plugins_url(CAAG_HQ_RENTAL_PLUGIN_FOLDER . '/assets/js/submit.js?timestamp=' . time()),
-        array('jquery'));
+    wp_enqueue_script('caag-rental-script-submit', plugin_dir_url(__FILE__) . 'assets/js/submit.js?timestamp=' . time(),
+        ['jquery']);
 }
 
 add_action('wp_enqueue_script', 'caag_hq_rental_inline_script');

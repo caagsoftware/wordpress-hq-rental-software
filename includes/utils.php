66f143d962a2208f0b92f85fba7ff2678a263329
Nadin Yamaui <nadin@caagcrm.com>
2017-12-24 10:04:50 -0400

    Updated version to skip cache

--- includes/utils.php
+++ includes/utils.php
@@ -6,7 +6,7 @@
  */
 function caag_hq_rental_styles()
 {
-    wp_register_style('caag-rental', plugin_dir_url(__FILE__) . '../assets/css/caag_hq_rental.css?version=1.0.11');
+    wp_register_style('caag-rental', plugin_dir_url(__FILE__) . '../assets/css/caag_hq_rental.css?version=1.0.12');
     wp_enqueue_style('caag-rental');
 }
 
@@ -21,7 +21,7 @@ function caag_hq_rental_scripts()
     //Registration
     wp_register_script('caag-rental-iframe-resize',
         plugin_dir_url(__FILE__) . '../assets/js/iframeSizer.min.js?version=3.5.15');
-    wp_register_script('caag-rental-iframe-init', plugin_dir_url(__FILE__) . '../assets/js/caagResize.js', array('jquery'));
+    wp_register_script('caag-rental-iframe-init', plugin_dir_url(__FILE__) . '../assets/js/caagResize.js?version=1.0.1', array('jquery'));
     
     //Enqueue
     wp_enqueue_script('caag-rental-iframe-resize');

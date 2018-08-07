e881431ea7822b4054248b575517e7c340849e2c
Faggioni <miguelfaggioni@gmail.com>
2018-08-07 13:44:01 -0400

    version 1.3.7.0

--- wordpress-hq-rental-software.php
+++ wordpress-hq-rental-software.php
@@ -1,12 +1,12 @@
 <?php
 /*
  * @package Wordpress HQ Rental Software
- * @version 1.3.6.8
+ * @version 1.3.7.0
  *
  *
 Plugin Name:  HQ Rental
 Description:  HQ Rental Software
-Version:      1.3.6.8
+Version:      1.3.7.0
 Author:       HQ Rental Software
 Author URI:   https://www.hqrentalsoftware.com
 License: GPLv3
@@ -17,7 +17,7 @@ License URI: https://www.gnu.org/licenses/gpl.html
 /*
  * Global Constant
  */
-define('CAAG_HQ_RENTAL_VERSION','1.3.6.8');
+define('CAAG_HQ_RENTAL_VERSION','1.3.7.0');
 define('CAAG_HQ_RENTAL_POST_TYPE','caag-rental-form');
 
 
